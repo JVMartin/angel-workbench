49a149af2d0a463d89b8397364320b0057d3a159
Jacob Martin <mindnumb@gmail.com>
2015-02-07 19:25:46 -0800

    Fixing artisan commands

---
+++ app/Console/Commands/AngelReset.php
@@ -0,0 +1,66 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class AngelReset extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'angel:reset';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Empty the database and run all package migrations.';
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$this->info('Emptying the database...');
+		$host      = Config::get('database.connections.mysql.host');
+		$database  = Config::get('database.connections.mysql.database');
+		$username  = Config::get('database.connections.mysql.username');
+		$password  = Config::get('database.connections.mysql.password');
+		$this->exec('mysql -h ' . $host . ' -u ' . $username . ' -p\'' . $password . '\' -e "DROP DATABASE ' . $database . '"');
+		$this->exec('mysql -h ' . $host . ' -u ' . $username . ' -p\'' . $password . '\' -e "CREATE DATABASE ' . $database . '"');
+
+		if (!is_dir(base_path('workbench/angel'))) {
+			$this->error('The workbench/angel directory does not exist.');
+			return;
+		}
+
+		chdir(base_path());
+
+		$this->info('Running all migrations...');
+		$this->exec('php artisan migrate --bench=angel/core');
+
+		foreach (glob(base_path('workbench/angel/*')) as $dir) {
+			if (!is_dir($dir)) {
+				$this->error('Not a directory: ' . $dir);
+				continue;
+			}
+			$package = basename($dir);
+			if ($package == 'core') continue;
+			$this->exec('php artisan migrate --bench=angel/' . $package);
+		}
+		$this->info('...all migrations have been ran.');
+	}
+
+	private function exec($command)
+	{
+		$this->info('Executing: ' . $command);
+		echo shell_exec($command);
+	}
+
+}
---
+++ app/Console/Commands/AngelWorkbench.php
@@ -0,0 +1,81 @@
+<?php namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Input\InputArgument;
+
+class AngelWorkbench extends Command {
+
+	/**
+	 * The console command name.
+	 *
+	 * @var string
+	 */
+	protected $name = 'angel:workbench';
+
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Update all workbench submodules.';
+
+	/**
+	 * Get the console command options.
+	 *
+	 * @return array
+	 */
+	protected function getOptions()
+	{
+		return array(
+			array('assets', 'a', InputOption::VALUE_NONE, 'Also publish assets.')
+		);
+	}
+
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$this->info('Updating all Angel submodules...');
+
+		if (!is_dir(base_path('workbench/angel'))) {
+			$this->error('The workbench/angel directory does not exist.');
+			return;
+		}
+
+		chdir(base_path());
+		$this->exec('git submodule init');
+		$this->exec('git submodule update');
+
+		if ($this->option('assets')) {
+			$this->exec('rm -rf public/packages/angel');
+		}
+
+		foreach (glob(base_path('workbench/angel/*')) as $dir) {
+			if (!is_dir($dir)) {
+				$this->error('Not a directory: ' . $dir);
+				continue;
+			}
+			$this->info('Entering ' . $dir);
+			chdir($dir);
+			$this->exec('git checkout master');
+			$this->exec('git pull');
+			if ($this->option('assets')) {
+				chdir(base_path());
+				$package = basename($dir);
+				$this->exec('php artisan asset:publish --bench=angel/' . $package);
+			}
+		}
+		$this->info('...all Angel submodules have been updated.');
+	}
+
+	private function exec($command)
+	{
+		$this->info('Executing: ' . $command);
+		echo shell_exec($command);
+	}
+
+}
